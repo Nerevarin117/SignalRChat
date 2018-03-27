24186086e57888b9a582ef8dfaac7eaef88d9976
Maxime GILLET <gilet666@hotmail.fr>
2018-03-27 16:26:58 +0200

    reactjs chat component now working, now need to work with component list

---
+++ SignalRChat/Scripts/Tutorial.jsx
@@ -0,0 +1,24 @@
+﻿/*var CommentBox = React.createClass({
+    render: function (name) {
+        return (
+            <div className="commentBox">
+                Hello, world ! I am a CommentBox.
+      </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('content')
+);*/
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">
+                Hello, world ! I am a CommentBox.
+      </div>
+        );
+    }
+}
